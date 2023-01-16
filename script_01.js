643b7ab4ae4f98465870ea6660bd76bbb64d4c31
Samot9 <thomas.merklinger@web.de>
2023-01-16 11:42:28 +0100

    16.01 11.40 Uhr

--- script_01.js
+++ script_01.js
@@ -9,3 +9,13 @@ const ageMark = 30;
 // Deklaration
 let isJohnOlder, isJohnEqual;
 
+//Test | Logische Aussage
+isJohnOlder = (ageJohn > ageMark);
+isJohnEqual = (ageJohn == ageMark);
+
+// Ausgabe
+console.log("ageJohn: " + ageJohn);
+console.log("ageMark: " + ageMark);
+console.log("isJohnOlder: " + isJohnOlder);
+console.log("isJohnEqual: " + isJohnEqual);
+console.log("------------------");
